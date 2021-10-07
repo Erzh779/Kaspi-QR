d7a4f46707eba0d8bc8c7a2fe36a1fb7fa7a73b8
Don Taipov <erzhan7taipov@mail.ru>
2021-10-07 15:13:29 +0600

    Initial Commit

---
+++ QR_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QR_App
+//
+//  Created by Erzhan Taipov on 10/7/21.
+//  Copyright © 2021 Erzhan Taipov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
